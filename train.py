a37f87d71885898c7ad61cc8dcdb193ea46eee3c
dronperminov <dronperminov@gmail.com>
2022-03-19 23:32:56 +0300

    add cross validation

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.model_selection import KFold
+from sklearn.base import clone
 
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
@@ -27,15 +29,61 @@ def get_data(dataset: pd.DataFrame, label_key: str = 'label') -> Tuple[pd.DataFr
     return features, labels
 
 
-def eval_model(name, model, features, test_labels, average='macro'):
+def eval_model(model, features, test_labels, average='macro'):
     pred_labels = model.predict(features)
 
-    precision = precision_score(test_labels, pred_labels, average=average)
-    recall = recall_score(test_labels, pred_labels, average=average)
-    f1 = f1_score(test_labels, pred_labels, average=average)
+    precision = precision_score(test_labels, pred_labels, average=average, zero_division=0)
+    recall = recall_score(test_labels, pred_labels, average=average, zero_division=0)
+    f1 = f1_score(test_labels, pred_labels, average=average, zero_division=0)
     acc = model.score(features, test_labels)
 
-    print(f'| {name:15} | {recall:6.4} | {precision:9.4} | {f1:8.4} | {acc:8.4} |')
+    return precision, recall, f1, acc
+
+
+def mean(values):
+    return sum(values) / len(values)
+
+
+def train(models, train_features, train_labels, test_features, test_labels):
+    for name, model in models.items():
+        model.fit(train_features, train_labels)
+
+    print('|           Модель           | Recall | Precision | macro f1 | Accuracy |')
+    print('|                        :-: |    :-: |       :-: |      :-: |      :-: |')
+
+    for name, model in models.items():
+        recall, precision, f1, acc = eval_model(model, test_features, test_labels)
+        print(f'| {name:26} | {recall:6.4} | {precision:9.4} | {f1:8.4} | {acc:8.4} |')
+
+
+def cross_val(models, features, labels, n_splits=10):
+    for model_name, model in models.items():
+        kfold = KFold(n_splits=n_splits)
+
+        recalls, precisions, f1s, accuracies = [], [], [], []
+
+        print(f'### {model_name}')
+        print('| Разбиение | Recall | Precision | macro f1 | Accuracy |')
+        print('|       :-: |    :-: |       :-: |      :-: |      :-: |')
+
+        for fold, (train_index, test_index) in enumerate(kfold.split(features)):
+            train_features, test_features = features.iloc[train_index], features.iloc[test_index]
+            train_labels, test_labels = labels[train_index], labels[test_index]
+
+            model = clone(model)
+            model.fit(train_features, train_labels)
+
+            name = f'{fold + 1} / {n_splits}'
+            recall, precision, f1, accuracy = eval_model(model, test_features, test_labels)
+
+            recalls.append(recall)
+            precisions.append(precision)
+            f1s.append(f1)
+            accuracies.append(accuracy)
+
+            print(f'| {name:9} | {recall:6.4} | {precision:9.4} | {f1:8.4} | {accuracy:8.4} |')
+
+        print(f'| в среднем | {mean(recalls):6.4} | {mean(precisions):9.4} | {mean(f1s):8.4} | {mean(accuracies):8.4} |\n')
 
 
 def main():
@@ -59,11 +107,11 @@ def main():
         'CatBoost': CatBoostClassifier(verbose=False, random_state=random_state),
     }
 
-    print('|      Model      | Recall | Precision | macro f1 | accuracy |')
-    print('+-----------------+--------+-----------+----------+----------+')
-    for name, model in models.items():
-        model.fit(train_features, train_labels)
-        eval_model(name, model, test_features, test_labels)
+    print('## Результаты кроссвалидации')
+    cross_val(models, features, labels)
+
+    print('## Результаты обучения')
+    train(models, train_features, train_labels, test_features, test_labels)
 
 
 if __name__ == '__main__':
